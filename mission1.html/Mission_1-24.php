c68b912be49055df73aa658911ab3b4dd78b7f9c
houmaiwakiri <mahomahoma888@gmail.com>
2021-12-17 23:37:02 +0900

    gogo

---
+++ mission1.html/Mission_1-24.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $str = "Hello World";
+        $filename="mission_1-24.txt";
+        $fp = fopen($filename,"a");
+        fwrite($fp, $str.PHP_EOL);
+        fclose($fp);
+        echo "書き込み成功！";
+    ?>
+</body>
+</html>
