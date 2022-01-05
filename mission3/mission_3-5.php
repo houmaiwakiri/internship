f515da120878f996797b4ab64c81a9d2af224bc2
houmaiwakiri <mahomahoma888@gmail.com>
2022-01-06 02:22:01 +0900

    削除のパスワード機能まで

--- mission3/mission_3-5.php
+++ mission3/mission_3-5.php
@@ -1,31 +1,35 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>mission3-4</title>
+    <title>mission3-5</title>
 </head>
 <body>
     <form action="" method="post">
-            <input type="text" name="name">
-            <input type="text" name="com">
-            <input type="submit" name="submit">
-            <input type="number" name="edit">
-            <input type="submit" name="submit" value="編集">
-            <br>
-            <input type="number" name="del" value="削除対象番号"> 
-            <input type="submit" name="submit" value="削除">
-            <br>
-            <!--formの閉じタグは下の方にある。-->
+        <span>お名前:</span><input type="text" name="name">
+        <span>コメント:</span><input type="text" name="com">
+        <span>パスワード:</span><input type="text" name="pass">
+        <input type="submit" name="submit">
+        <br>
+        <span>削除番号指定:</span><input type="number" name="del" value="削除対象番号"> 
+        <span>パスワード:</span><input type="text" name="del_pass">
+        <input type="submit" name="submit" value="削除">
+        <br>
+        <span>編集番号指定:</span><input type="number" name="edit">
+        <span>パスワード:</span><input type="text" name="edit_pass">
+        <input type="submit" name="submit" value="編集">
+        <br>
+        <!--formの閉じタグは下の方にある。-->
     <?php
-        $filename = "mission_3-4.txt";//ファイルの名前を決める
-        $date = date("Y年m月d日 H時i分s秒");
-        if(!empty($_POST["name"]) && !empty($_POST["com"])){
+        $filename = "mission_3-5.txt";//ファイルの名前を決める
+        $date = date("Y年m月d日H時i分s秒");
+        if(!empty($_POST["name"]) && !empty($_POST["com"])&& !empty($_POST["pass"])){
             $fp = fopen($filename,"a");//追記モードで開く。
             $name = $_POST["name"];
             $com = $_POST["com"];
-
+            $pass=$_POST["pass"];
             if(file_exists($filename)){
                 //投稿番号
                 $lines=file($filename);//($filename,FILE_SKIP_EMPTY_LINES)
@@ -34,21 +38,24 @@
             }else{
                $num = 1;
             }
-            $tex=$num."<>".$name."<>".$com."<>".$date;
+            $tex=$num."<>".$name."<>".$com."<>".$date."<>".$pass."<>";
             fwrite($fp,$tex.PHP_EOL);
             fclose($fp);
-        }elseif(!empty($_POST["del"]) && empty($name) && empty($com)&& empty($_POST["edit"])){
+        }elseif(!empty($_POST["del"]) &&!empty($_POST["del_pass"])&& empty($name) && empty($com)&& empty($_POST["edit"])&&empty($_POST["pass"])){
             //削除機能
             $del = $_POST["del"];//削除ナンバー受け取り
-            if(!empty($_POST["del"])){
-                $delnum=file($filename);
-                for($i=0; $i<count($delnum); $i++){
-                    $delData = explode("<>",$delnum[$i]);
-                    //array_splice ( $配列, $開始位置[, $削除する要素の数 [, $置き換える要素を含んだ配列 ]] )
-                    if($delData[0]==$del){
-                        array_splice($delnum,$i,1);
-                        file_put_contents($filename, implode("",$delnum));
-                    }
+            $del_pass=$_POST["del_pass"];//削除パスワード
+            $lines=file($filename,FILE_SKIP_EMPTY_LINES);
+            $lastline= count($lines);
+            $delnum=file($filename);
+            $ok=false;
+            for($i=0; $i<count($delnum); $i++){
+                $delData = explode("<>",$delnum[$i]);
+                //array_splice ( $配列, $開始位置[, $削除する要素の数 [, $置き換える要素を含んだ配列 ]] )
+                if($delData[4]==$del_pass&&$delData[0]==$del){
+                    array_splice($delnum,$i,1);
+                    file_put_contents($filename, implode("",$delnum));
+                }else{                    
                 }
             }
         }elseif(!empty($_POST["edit"]) && empty($name) && empty($com)&& empty($_POST["del"])){
@@ -68,8 +75,8 @@
                   $edit_date=$editData[3];
                   $edit_num=$editData[0];
                   echo
-                  '<input type="text" name="edit_name" value="'.$edit_name.'"></input><br>
-                  <input type="text" name="edit_com" value="'.$edit_com.'"></input>
+                  '<span>お名前:</span><input type="text" name="edit_name" value="'.$edit_name.'"></input><br>
+                  <span>コメント:</span> <input type="text" name="edit_com" value="'.$edit_com.'"></input>
                   <input type="hidden" name="edit_num" value="'.$edit_num.'"></input>
                   <input type="submit" name="submit" value="編集する"><br>
                   </form>';
@@ -88,7 +95,7 @@
                 if($tex[0]!=$edit_num){
                 fwrite($fp,$line);
                 }else{
-                fwrite($fp,$edit_num."<>".$edit_name."<>".$edit_com."<>".$date."<>".$tex[4].PHP_EOL);
+                fwrite($fp,$edit_num."<>".$edit_name."<>".$edit_com."<>".$date."<>".$tex[3].PHP_EOL);
                 }
             }fclose($fp);
         }elseif(isset($_POST["edit_num"])&&(empty($_POST["edit_name"])||empty($_POST["edit_com"])) ){
