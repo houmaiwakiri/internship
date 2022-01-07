b706f2b52633e3d7e167af2e80b2bde6a1eaac63
houmaiwakiri <mahomahoma888@gmail.com>
2022-01-07 21:06:29 +0900

    gitignore使ってみた。

---
+++ mission4/mission_4-8.php
@@ -0,0 +1,64 @@
+<?php
+    //接続設定4-1
+    $dsn = 'mysql:dbname=tb230882db;host=localhost';
+    $user = 'tb-230882';
+    $password = 'UdJfTfs5wT';
+    $pdo = new PDO($dsn, $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
+    //テーブル作成4-2
+    $sql = "CREATE TABLE IF NOT EXISTS tbtest"
+    ." ("
+    . "id INT AUTO_INCREMENT PRIMARY KEY,"
+    . "name char(32),"
+    . "comment TEXT"
+    .");";
+    $stmt = $pdo->query($sql);
+    //テーブル表示4-3
+    $sql ='SHOW TABLES';
+    $result = $pdo -> query($sql);
+    foreach ($result as $row){
+        echo $row[0];
+        echo '<br>';
+    }
+    echo "<hr>";
+    //テーブル構成4-4
+    $sql ='SHOW CREATE TABLE tbtest';
+    $result = $pdo -> query($sql);
+    foreach ($result as $row){
+        echo $row[1];
+    }
+    echo "<hr>";
+    //データ登録4-5
+    $sql = $pdo -> prepare("INSERT INTO tbtest (name, comment) VALUES (:name, :comment)");
+    $sql -> bindParam(':name', $name, PDO::PARAM_STR);
+    $sql -> bindParam(':comment', $comment, PDO::PARAM_STR);
+    $name = 'ぼくです。';
+    $comment = 'お腹が空いたのでペヤングを食べました。'; //好きな名前、好きな言葉は自分で決めること
+    $sql -> execute();
+    //データ表示4-6
+    $sql = 'SELECT * FROM tbtest';
+    $stmt = $pdo->query($sql);
+    $results = $stmt->fetchAll();
+    foreach ($results as $row){
+        //$rowの中にはテーブルのカラム名が入る
+        echo $row['id'].',';
+        echo $row['name'].',';
+        echo $row['comment'].'<br>';
+    echo "<hr>";
+    }
+    //データレコードを編集4-7
+    $id = 1; //変更する投稿番号
+    $name = "ボブ";
+    $comment = "ラーメンガタベタイイ"; //変更したい名前、変更したいコメントは自分で決めること
+    $sql = 'UPDATE tbtest SET name=:name,comment=:comment WHERE id=:id';
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':name', $name, PDO::PARAM_STR);
+    $stmt->bindParam(':comment', $comment, PDO::PARAM_STR);
+    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+    $stmt->execute();
+    //データを消す。4-8
+    $id = 2;
+    $sql = 'delete from tbtest where id=:id';
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+    $stmt->execute();
+?>
